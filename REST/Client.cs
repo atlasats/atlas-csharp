28fa92c35d52b7222b3fcc9679183a18a6d0a6d7
Habeel Ahmed <ahabeel@gmail.com>
2014-08-30 01:19:18 -0400

    added a REST client for spinup

--- REST/Client.cs
+++ REST/Client.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,59 @@ namespace Atlas.REST
 {
     public class Client
     {
-        public Client(string url)
-        {
+        private RestClient client;
+        private string token;
 
+        public Client(string url, string token)
+        {
+            client = new RestClient(url);
+            this.token = token;
+        }
+
+        private string get(string path, Dictionary<string, object> parameters, bool authenticated=false)
+        {
+            var request = new RestRequest(path, Method.GET);
+            if (parameters != null)
+            {
+                foreach (var key in parameters.Keys)
+                {
+                    request.AddParameter(key, parameters[key]);
+                }
+            }
+
+            if (authenticated)
+            {
+                request.AddHeader("Authorization", string.Format("Token token=\"{0}\"", token));
+            }
+
+            var response = client.Execute(request);
+
+            return response.Content;
+        }
+
+        public string Book(string item, string currency)
+        {
+            return get("api/v1/market/book", new Dictionary<string, object> { { "item", item }, { "currency", currency } });
+        }
+
+        public string RecentTrades(string item, string currency)
+        {
+            return get("api/v1/market/trades/recent", new Dictionary<string, object> { { "item", item }, { "currency", currency } });
+        }
+
+        public string RecentOrders()
+        {
+            return get("api/v1/orders", null, true);
+        }
+
+        public string OrderInfo(string orderid)
+        {
+            return get(string.Format("api/v1/orders/{0}", orderid), null, true);
+        }
+
+        public string AccountInfo()
+        {
+            return get("api/v1/account", null, true);
         }
     }
 }
